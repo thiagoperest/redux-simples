1a553b7a2b0e8cea50cf68ba068cf615257d60c5
thiagoperest <thiago_teodoroperes07@hotmail.com>
2022-05-14 18:38:32 -0300

    Cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Card from "./components/card/Card";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Exercício React-Redux (Simples)</h1>
+      <div className="linha">
+        <Card title="Card 1" red>
+          1
+        </Card>
+        <Card title="Card 2" green>
+          2
+        </Card>
+      </div>
+      <div className="linha">
+        <Card title="Card 3" blue>
+          3
+        </Card>
+        <Card title="Card 4" purple>
+          4
+        </Card>
+      </div>
     </div>
   );
 }
